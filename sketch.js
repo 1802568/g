50a7627b0acf78483bdab5274c7a6734390ff9cd
Qazasd Rtyrfv Ujmuiop Asbitfucshniggpenrap <76923976+1802568@users.noreply.github.com>
2021-12-23 21:05:28 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, box2, box3,box4;
+var hero,rope,ground;
+var score=0
+function preload() {
+  
+}
+
+function setup() {
+  createCanvas(3000, 700);
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground = new Ground(600, 600, 1900, 20);
+  ground1 = new Ground(800, 535, 20, 150);
+  ground2 = new Ground(1200, 535, 20, 150);
+  hero = new Hero(400,800,250);
+  rope = new Rope(hero.body, { x: 500, y: 50 });
+  
+
+  
+
+}
+
+function draw() {
+  background("black");
+  Engine.update(engine);
+ ground1.display()
+  ground.display();
+  ground2.display()
+ 
+
+  hero.display();
+  rope.display();
+  
+
+}
+function mouseDragged(){
+  Matter.Body.setPosition(hero.body, {x: mouseX , y: mouseY});
+  strokeWeight(3)
+}
+
+
+function mouseReleased(){
+  rope.fly();
+  hero.velocityX=5
+}
